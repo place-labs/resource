d5bb1bb953c353fd5a471ba915f63867c2ae287d
Caspian Baska <caspianbaska@gmail.com>
2020-08-18 17:03:29 +1000

    test: minimal specs

--- spec/helper.cr
+++ spec/helper.cr
@@ -1,2 +1,33 @@
 require "spec"
 require "../src/placeos-resource"
+
+require "action-controller/logger"
+require "rethinkdb-orm"
+
+Spec.before_suite do
+  Basic.clear
+  Log.builder.bind("*", backend: ActionController.default_backend, level: Log::Severity::Debug)
+end
+
+class Basic < RethinkORM::Base
+  attribute name : String
+end
+
+class Processor < PlaceOS::Resource(Basic)
+  getter creates = [] of String
+  getter updates = [] of String
+  getter deletes = [] of String
+
+  def process_resource(action : PlaceOS::Resource::Action, resource : Basic) : PlaceOS::Resource::Result
+    case action
+    in PlaceOS::Resource::Action::Created
+      creates << resource.name
+    in PlaceOS::Resource::Action::Updated
+      updates << resource.name
+    in PlaceOS::Resource::Action::Deleted
+      deletes << resource.name
+    end
+
+    PlaceOS::Resource::Result::Success
+  end
+end
--- spec/placeos-resource_spec.cr
+++ spec/placeos-resource_spec.cr
@@ -1,6 +1,96 @@
 require "./helper"
+require "uuid"
 
 module PlaceOS
   describe Resource do
+    it "processes all resources on start up", tags: "resource" do
+      models = Array(Basic).new(5) { Basic.new(name: UUID.random.to_s).save! }
+
+      processor = Processor.new
+      processor.start
+      processor.creates.size.should eq(5)
+
+      processor.stop
+
+      models.each &.destroy
+
+      Fiber.yield
+
+      processor.deletes.should be_empty
+    end
+
+    it "listens for changes on resources", tags: "resource" do
+      processor = Processor.new.start
+      processor.creates.should be_empty
+      processor.updates.should be_empty
+      processor.deletes.should be_empty
+
+      create_name = UUID.random.to_s
+      update_name = UUID.random.to_s
+      model = Basic.new(name: create_name).save!
+
+      Fiber.yield
+
+      processor.creates.size.should eq 1
+      processor.updates.should be_empty
+      processor.deletes.should be_empty
+
+      processor.creates.first.should eq create_name
+
+      model.name = update_name
+      model.save!
+
+      Fiber.yield
+
+      processor.creates.size.should eq 1
+      processor.updates.size.should eq 1
+      processor.deletes.should be_empty
+
+      processor.updates.first.should eq update_name
+
+      model.destroy
+
+      Fiber.yield
+
+      processor.creates.size.should eq 1
+      processor.updates.size.should eq 1
+      processor.deletes.size.should eq 1
+
+      processor.deletes.first.should eq update_name
+
+      processor.stop
+    end
+
+    it "reconnects if db connection is lost", tags: "retry" do
+      create_name = UUID.random.to_s
+      update_name = UUID.random.to_s
+      model = Basic.new(name: create_name).save!
+      processor = Processor.new.start
+
+      processor.creates.size.should eq 1
+
+      begin
+        RethinkORM::Connection.db.@sock.close
+      rescue
+      end
+
+      # Retry until connection is up again
+      SimpleRetry.try_to do
+        begin
+          Basic.count
+        rescue
+          raise "retrying connection"
+        end
+      end
+
+      model.name = update_name
+      model.save!
+
+      Fiber.yield
+
+      processor.updates.size.should eq 1
+      processor.stop
+      model.destroy
+    end
   end
 end
