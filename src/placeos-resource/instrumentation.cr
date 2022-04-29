c73ccb1ba231b952b96d14f0e2dd94192ce53196
Caspian Baska <email@caspian.computer>
2022-04-30 09:13:44 +1000

    build: add `defined` shard

--- src/placeos-resource/instrumentation.cr
+++ src/placeos-resource/instrumentation.cr
@@ -1,3 +1,5 @@
+require "defined"
+
 # This allows opt-out of specific instrumentation at compile time, via environment variables.
 # Refer to https://wyhaines.github.io/defined.cr/ for details about all supported check types.
 unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_PLACEOS_RESOURCE") do
