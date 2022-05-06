b9d0b247c33585dbff40244dc4b53f99be3e052c
Caspian Baska <email@caspian.computer>
2022-05-06 13:29:44 +1000

    refactor(telemetry): instrument `process_resource` instead of `_process_resource`

--- src/placeos-resource/telemetry.cr
+++ src/placeos-resource/telemetry.cr
@@ -1,31 +1,25 @@
-require "defined"
+module OpenTelemetry::Instrumentation
+  class PlaceOSResource < OpenTelemetry::Instrumentation::Instrument
+  end
+end
 
-# This allows opt-out of specific instrumentation at compile time, via environment variables.
-# Refer to https://wyhaines.github.io/defined.cr/ for details about all supported check types.
-unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_PLACEOS_RESOURCE") do
-  if_defined?(OpenTelemetry::Instrumentation::Instrument) do
-    module OpenTelemetry::Instrumentation
-      class PlaceOSResource < OpenTelemetry::Instrumentation::Instrument
-      end
+abstract class PlaceOS::Resource(T)
+  trace("load_resources") do
+    OpenTelemetry.trace.in_span("#{self.class.name} Load Resources") do
+      previous_def
     end
+  end
 
-    abstract class PlaceOS::Resource(T)
-      trace("load_resources") do
-        OpenTelemetry.trace.in_span("#{self.class.name} Load Resources") do
-          previous_def
-        end
-      end
+  trace("start") do
+    OpenTelemetry.trace.in_span("#{self.class.name} Start") do
+      previous_def
+    end
+  end
 
-      trace("start") do
-        OpenTelemetry.trace.in_span("#{self.class.name} Start") do
-          previous_def
-        end
-      end
-
-      trace("_process_event") do
-        OpenTelemetry.trace.in_span("#{self.class.name} Process Event") do
-          previous_def
-        end
+  macro inherited
+    trace("process_event") do
+      OpenTelemetry.trace.in_span("#{self.class.name} Process Event") do
+        previous_def
       end
     end
   end
